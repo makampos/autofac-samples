62790be753e9f8c478d027b5a6692c6eb4452183
Matheus <matheus.campos@edu.unifil.br>
2023-01-24 23:23:07 +0100

    passing parameters to register

--- src/Autofac.Samples/Program.cs
+++ src/Autofac.Samples/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Autofac.samples
+﻿using Autofac.Core;
+
+namespace Autofac.samples
 {
     public interface ILog
     {
@@ -34,9 +36,28 @@
             id = new Random().Next();
         }
 
+        public Engine(ILog log, int id)
+        {
+            this.log = log;
+            this.id = id;
+        }
+
         public void Ahead(int power)
         {
             log.Write($"Engine[{id}] ahead {power}");
+        }
+    }
+
+    public class SMSLog : ILog
+    {
+        private string phoneNumber;
+        public SMSLog(string phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+        public void Write(string message)
+        {
+            Console.WriteLine($"SMS to {phoneNumber} : {message}");
         }
     }
     public class Car
@@ -44,6 +65,11 @@
         private Engine engine;
         private ILog log;
 
+        public Car(Engine engine)
+        {
+            this.engine = engine;
+            this.log = new EmailLog();
+        }
         public Car(Engine engine, ILog log)
         {
             this.engine = engine;
@@ -62,17 +88,46 @@
         public static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
-            builder.RegisterType<EmailLog>()
-                .As<ILog>()
-                .As<IConsole>();
-            builder.RegisterType<ConsoleLog>().As<ILog>().PreserveExistingDefaults();
-            builder.RegisterType<Engine>();
-            builder.RegisterType<Car>();
-            IContainer container = builder.Build();
+            // named parameter
+            // builder.RegisterType<SMSLog>()
+            //     .As<ILog>()
+            //     .WithParameter("phoneNumber", "+123456789");
+            
+            //type parameter
+            // builder.RegisterType<SMSLog>()
+            //     .As<ILog>()
+            //     .WithParameter(new TypedParameter(typeof(string), "_123456789"));
+            
+            
+            // builder.RegisterType<SMSLog>()
+            //     .As<ILog>()
+            //     .WithParameter(
+            //         // predicate
+            //         new ResolvedParameter(
+            //     (pi, ctx) => pi.ParameterType == typeof(string) && pi.Name == "phoneNumber",
+            //             // value acessor
+            // (pi,ctx) => "+123456678"
+            //         )
+            //      );
+            //
+            // var container = builder.Build();
+            // var log = container.Resolve<ILog>();
+            // log.Write($"test message");
 
-            var car = container.Resolve<Car>();
 
-            car.Go();
+            Random random = new Random();
+
+            builder.Register
+            ((c, p)
+                => new SMSLog(p.Named<string>($"phoneNumber")))
+                .As<ILog>();
+            
+            Console.WriteLine($"about to build container...");
+            var container = builder.Build();
+
+            var log = container.Resolve<ILog>
+                (new NamedParameter($"phoneNumber", random.Next().ToString()));
+            log.Write($"testing");
         }
     }
 }
